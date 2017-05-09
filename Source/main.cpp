71c82a1e154aa2e7e171140c6bd21af091623032
Supersil <sasha-silkov@yandex.ru>
2017-05-09 15:22:08 +0300

    Commit test

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,12 +3,15 @@
 #include "person.h"
 #include "mainwindow.h"
 
+// test
+
 int main(int argc, char *argv[])
 {
 
 	QApplication a(argc, argv);
 //	Widget w;
 //	w.show();
+
 
 	MainWindow m;
 	m.show();
